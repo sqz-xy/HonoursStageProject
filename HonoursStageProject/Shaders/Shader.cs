7a7713493cf549cf584d8ca761cb8e22d5315acb
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2022-10-19 18:55:40 +0100

    Added shader code

--- HonoursStageProject/Shaders/Shader.cs
+++ HonoursStageProject/Shaders/Shader.cs
@@ -1,4 +1,9 @@
-﻿namespace HonoursStageProject.Shaders;
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL;
+namespace HonoursStageProject.Shaders;
 
 public class Shader
 {
