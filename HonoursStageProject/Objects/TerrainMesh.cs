50bc38e37c33e698915e5e9a5d664b614f36ed6f
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2022-11-07 22:51:54 +0000

    Added more comments

--- HonoursStageProject/Objects/TerrainMesh.cs
+++ HonoursStageProject/Objects/TerrainMesh.cs
@@ -26,9 +26,11 @@ public class TerrainMesh : Shape
         
         int vertexPointer = 0;
         Random rand = new Random();
+        
         for (int i = 0; i < pHeight; i++)
         for (int j = 0; j < pWidth; j++)
         {
+            // Starts top left corner, ends bottom right , -width/2 to +width/2. -Dim/2 centres the mesh on 0,0, add i to move along the dimension
             Vertices[vertexPointer * stride] = (-pHeight / 2.0f) + i; // X, the range of the X dimension
             Vertices[(vertexPointer * stride) + 1] = 0; //(float) rand.NextDouble();                // Y - Height will be added later
             Vertices[(vertexPointer * stride) + 2] = (-pWidth / 2.0f) + j; // Z, the range of the Z dimension
@@ -45,7 +47,7 @@ public class TerrainMesh : Shape
                 // Triangle strip vertices are ordered from top row to bottom row
                 // Alternate between row i and i + 1, j is the columns
                 // j + pWidth is current column + width, i + k is alternating between the rows
-                //int test = i + k;
+                // int test = i + k;
                 
                 //                        Across Columns * Across Row
                 Indices[indicesPointer] = (uint) (j + pWidth * (i + k));
