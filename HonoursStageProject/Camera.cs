a11a1bb0477833f4d7001c7864e7c2079d9082de
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2022-11-09 15:40:31 +0000

    Added basic camera class with wasd movement

---
+++ HonoursStageProject/Camera.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.InteropServices.ObjectiveC;
+using HonoursStageProject.Managers;
+using OpenTK;
+
+namespace HonoursStageProject;
+
+public enum Direction
+{
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+}
+
+public class Camera
+{
+    public Matrix4 View;
+    public Matrix4 Projection;
+
+    private Vector3 _position;
+    private Vector3 _target;
+    private Vector3 _up;
+    private Vector3 _right;
+    private Vector3 _direction;
+
+    public Camera()
+    {
+        _position = new Vector3(0.0f, 0.5f, 0.5f);
+        _target = new Vector3(0.0f, 0.0f, 0.0f);
+        _up = new Vector3(0.0f, 1.0f,  0.0f);
+        _direction = Vector3.Normalize(_target - _position);
+        _right = Vector3.Normalize(Vector3.Cross(_up, _direction));
+            
+        View = Matrix4.CreateTranslation(0, 0, -2);
+        Projection = Matrix4.CreatePerspectiveFieldOfView(1, (float)SceneManager.SWidth / SceneManager.SHeight, 0.5f, 5);
+        
+        UpdateCamera();
+    }
+
+    public void MoveCamera(Direction pDirection, float pDistance)
+    {
+        switch (pDirection)
+        {
+            case Direction.FORWARD:
+                _position += _direction * pDistance;
+                break;
+            case Direction.BACKWARD:
+                _position -= _direction * pDistance;
+                break;
+            case Direction.LEFT:
+                _position += _right * pDistance;
+                break;
+            case Direction.RIGHT:
+                _position -= _right * pDistance;
+                break;
+        }
+        UpdateCamera();
+    }
+
+    public void UpdateCamera()
+    {
+        View = Matrix4.LookAt(_position, _position + _direction, _up); 
+    }
+}
--- HonoursStageProject/Scenes/TerrainScene.cs
+++ HonoursStageProject/Scenes/TerrainScene.cs
@@ -64,6 +64,7 @@ public class TerrainScene : Scene
     };
 
     private int _cubeIndex;
+    private Camera _camera;
     
 
     // Camera
@@ -92,53 +93,34 @@ public class TerrainScene : Scene
         // Add toggle for this
         GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
         
-        VertexManager.Initialize(1, 1, 1);
-
-
-        _terrainMesh = new TerrainMesh(100, 100, 10);
+        // Camera and Shader initialization
+        _camera = new Camera();
         _shader = new Shader(@"Shaders/terrainscene.vert", @"Shaders/terrainscene.frag");
-        _modelMatrix = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(0.0f)); // Changed this to see correct side of mesh
-        _viewMatrix = Matrix4.CreateTranslation(0.0f, 10.0f, -10.0f);
-        _projectionMatrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), SceneManager.Width / SceneManager.Height, 0.01f, 100.0f);
         
-        _square = new Quadrilateral(new Vector2(0.0f, 0.0f),0.2f, 0.1f, Vector4.One)
-        {
-            Colour = Vector4.One,
-        };
-        
-        //_square.Index = VertexManager.BindVertexData(_square.Vertices, _square.Indices, 0, 0, 0);
+        VertexManager.Initialize(1, 1, 1);
+        _terrainMesh = new TerrainMesh(100, 100, 10);
         _terrainMesh.Index = VertexManager.BindVertexData(_terrainMesh.Vertices, _terrainMesh.Indices, 0, 0, 0);
-        
-        // Camera
-        
-        _viewMatrix = Matrix4.CreateTranslation(0, 0, -2); 
-        Vector3 eye = new Vector3(0.0f, 0.5f, 0.5f);  
-        Vector3 lookAt = new Vector3(0, 0, 0);  
-        Vector3 up = new Vector3(0, 1, 0); 
-        _viewMatrix = Matrix4.LookAt(eye, lookAt, up); 
-        
-        int uProjectionLocation = GL.GetUniformLocation(_shader.Handle, "uProjection"); 
-        // Add to resize method
-        _projectionMatrix = Matrix4.CreatePerspectiveFieldOfView(1, (float)SceneManager.Width / SceneManager.Height, 0.5f, 5); 
-        GL.UniformMatrix4(uProjectionLocation, true, ref _projectionMatrix); 
+        _modelMatrix = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(0.0f)); // Changed this to see correct side of mesh
         
         GL.UseProgram(_shader.Handle);
         
+        UpdateMatrices();
+    }
+
+    private void UpdateMatrices()
+    {
         int uModel = GL.GetUniformLocation(_shader.Handle, "uModel");
         GL.UniformMatrix4(uModel, true, ref _modelMatrix);
-        
+
         int uView = GL.GetUniformLocation(_shader.Handle, "uView");
-        GL.UniformMatrix4(uView, true, ref _viewMatrix);
-        
+        GL.UniformMatrix4(uView, true, ref _camera.View);
+
         int uProjection = GL.GetUniformLocation(_shader.Handle, "uProjection");
-        GL.UniformMatrix4(uProjection, true, ref _projectionMatrix);
-        
+        GL.UniformMatrix4(uProjection, true, ref _camera.Projection);
     }
 
     public override void Render(FrameEventArgs e)
     {
-        //Console.WriteLine("Rendering");
-        
         _shader.UseShader();
         
         GL.BindVertexArray(VertexManager.GetVaoAtIndex(_terrainMesh.Index));
@@ -147,58 +129,31 @@ public class TerrainScene : Scene
 
     public override void Update(FrameEventArgs e)
     {
-        int vertexColorLocation = GL.GetUniformLocation(_shader.Handle, "uColour");
-        GL.Uniform4(vertexColorLocation, new Color4(0.0f, 0.0f, 0.5f, 1.0f));
+        UpdateMatrices();
     }
-
-    private void GenerateMesh(int pWidth, int pHeight, int pResolution, out float[] pMeshVertices, out uint[] pMeshIndices)
-    {
-        pMeshVertices = new float[] { };
-        pMeshIndices = new uint[] { };
-    }
-
+    
     private void KeyPress(KeyPressEventArgs e)
     {
         if (e.KeyChar == 'a') 
         { 
-            _viewMatrix = _viewMatrix * Matrix4.CreateTranslation(0.01f, 0, 0); 
-            int uViewLocation = GL.GetUniformLocation(_shader.Handle, "uView"); 
-            GL.UniformMatrix4(uViewLocation, true, ref _viewMatrix); 
+            _camera.MoveCamera(Direction.LEFT, 0.1f);
         }
         else if (e.KeyChar == 'd') 
         { 
-            _viewMatrix = _viewMatrix * Matrix4.CreateTranslation(-0.01f, 0, 0); 
-            int uViewLocation = GL.GetUniformLocation(_shader.Handle, "uView"); 
-            GL.UniformMatrix4(uViewLocation, true, ref _viewMatrix); 
+            _camera.MoveCamera(Direction.RIGHT, 0.1f);
         }
         else if (e.KeyChar == 'w') 
         { 
-            _viewMatrix = _viewMatrix * Matrix4.CreateTranslation(0.0f, 0.0f, 0.01f); 
-            int uViewLocation = GL.GetUniformLocation(_shader.Handle, "uView"); 
-            GL.UniformMatrix4(uViewLocation, true, ref _viewMatrix); 
+            _camera.MoveCamera(Direction.FORWARD,0.1f);
         }
         else if (e.KeyChar == 's') 
         { 
-            _viewMatrix = _viewMatrix * Matrix4.CreateTranslation(0.0f, 0.0f, -0.01f); 
-            int uViewLocation = GL.GetUniformLocation(_shader.Handle, "uView"); 
-            GL.UniformMatrix4(uViewLocation, true, ref _viewMatrix); 
-        }
-        else if (e.KeyChar == 'q')
-        {
-            _viewMatrix = _viewMatrix * Matrix4.CreateRotationY(0.1f);
-            int uViewLocation = GL.GetUniformLocation(_shader.Handle, "uView"); 
-            GL.UniformMatrix4(uViewLocation, true, ref _viewMatrix); 
-        }
-        else if (e.KeyChar == 'e') 
-        { 
-            _viewMatrix = _viewMatrix * Matrix4.CreateRotationY(-0.1f);
-            int uViewLocation = GL.GetUniformLocation(_shader.Handle, "uView"); 
-            GL.UniformMatrix4(uViewLocation, true, ref _viewMatrix); 
+            _camera.MoveCamera(Direction.BACKWARD,0.1f);
         }
     }
 
     public override void Close()
     {
-        
+        VertexManager.ClearData();
     }
 }
